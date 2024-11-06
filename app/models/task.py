6bfc294db9d6f3f5b697d4072560aa43099957c7
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-11-05 23:34:16 -0500

    created Task model

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,18 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
+from typing import Optional
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]]
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            title=self.title,
+            description=self.description,
+            is_complete=bool(self.completed_at)
+        )
